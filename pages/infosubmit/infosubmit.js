705003a1c136736027464fdb5285e77622c8fdc4
1 <1>
2019-04-26 16:32:34 +0800

    已修改        app.json 已添加        images/logo.png 已添加        images/logo_tit.png 已添加        pages/chooseskill/chooseskill.js 已添加        pages/chooseskill/chooseskill.json 已添加        pages/chooseskill/chooseskill.wxml 已添加        pages/chooseskill/chooseskill.wxss 已修改        pages/index/index.wxml 已修改        pages/index/index.wxss 已添加        pages/infosubmit/infosubmit.js 已添加        pages/infosubmit/infosubmit.json 已添加        pages/infosubmit/infosubmit.wxml 已添加        pages/infosubmit/infosubmit.wxss 已修改        app.wxss Add        font/iconfont.wxss

---
+++ pages/infosubmit/infosubmit.js
@@ -0,0 +1,45 @@
+// pages/infosubmit.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    sex:[
+      {
+        name:"男",
+        checked:false
+      },
+      {
+        name: "女",
+        checked: false
+      }
+    ],
+    date: '1990-01-01',
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  radioChange(e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+
+  bindDateChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+})
